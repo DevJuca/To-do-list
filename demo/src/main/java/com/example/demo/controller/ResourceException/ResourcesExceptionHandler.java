cac0042c79b4287aa398fe4be46cfdeb1cbf354d
João Carlos <justodeandrade@gmail.com>
2025-08-13 16:51:58 -0300

    ResourcePriorityException, ResourceStatusException e ResourceDeadlineException

--- demo/src/main/java/com/example/demo/controller/ResourceException/ResourcesExceptionHandler.java
+++ demo/src/main/java/com/example/demo/controller/ResourceException/ResourcesExceptionHandler.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.service.ResourcesException.DataBaseException;
+import com.example.demo.service.ResourcesException.ResourcePriorityException;
+import com.example.demo.service.ResourcesException.ResourceStatusException;
+import com.example.demo.service.ResourcesException.ResourcesDeadlineException;
 import com.example.demo.service.ResourcesException.ResourcesNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +20,33 @@ public class ResourcesExceptionHandler {
 
     @ExceptionHandler(ResourcesNotFoundException.class)
     public ResponseEntity<StandardException> resourceExcepiton(ResourcesNotFoundException e,
+            HttpServletRequest request) {
+        String error = "Resources Not Found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardException err = new StandardException(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ResourcesDeadlineException.class)
+    public ResponseEntity<StandardException> resourceExcepiton(ResourcesDeadlineException e,
+            HttpServletRequest request) {
+        String error = "Resources Not Found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardException err = new StandardException(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ResourcePriorityException.class)
+    public ResponseEntity<StandardException> resourceExcepiton(ResourcePriorityException e,
+            HttpServletRequest request) {
+        String error = "Resources Not Found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardException err = new StandardException(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(ResourceStatusException.class)
+    public ResponseEntity<StandardException> resourceExcepiton(ResourceStatusException e,
             HttpServletRequest request) {
         String error = "Resources Not Found";
         HttpStatus status = HttpStatus.NOT_FOUND;
